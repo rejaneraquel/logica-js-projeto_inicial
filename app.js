3c3f1821abd95e6d6600eb243a1f0f092a982ce0
Rejane Lima <rejane.lima87@gmail.com>
2024-11-17 14:10:10 -0300

    Organizando git da pasta Alura

--- app.js
+++
@@ -1,7 +0,0 @@
-alert('Seja bem vindo(a) ao jogo do número secreto');
-let numeroSecreto = 29;
-let chute = prompt('Escolha um número entre 1 e 30')
-
-if (numeroSecreto == chute) {
-  console.log(`Parabéns, você acertou o número secreto ${chute}`)
-} else { console.log(`Tente outra vez, não é o número ${chute}`)}
